eb3b2db054cc8be9eded5307c3f98fa2cb774211
Gurucharan M <124780867+GURUCHARAN-M@users.noreply.github.com>
2024-08-13 13:58:22 +0530

    Delete Day_16_Frontend_Integration directory

--- Day_16_Frontend_Integration/frontend/src/styles.js
+++
@@ -1,6 +0,0 @@
-
-export const styles = {
-    sectionSubText: "text-[18px] font-semibold text-tertiary uppercase tracking-wider",
-    sectionHeadText: "text-[32px] font-bold text-primary",
-  };
-  
