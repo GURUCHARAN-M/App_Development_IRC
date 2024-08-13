62bc95d5fda744cbb489e64298d17128b2007273
Gurucharan M <124780867+GURUCHARAN-M@users.noreply.github.com>
2024-08-13 13:58:05 +0530

    Delete Day_15_API_Gateway directory

--- Day_15_API_Gateway/src/main/java/com/example/backend/repository/DataRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.DataModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataRepo extends JpaRepository<DataModel,Integer> {
-
-}
